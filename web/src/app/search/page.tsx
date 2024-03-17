ed2141d5a503266a3f8c462a21f7b923ba3e41a0
wangjun <jun.wangj@me.com>
2024-03-17 22:07:58 +0800

    修改样式

--- web/src/app/search/page.tsx
+++ web/src/app/search/page.tsx
@@ -28,7 +28,7 @@ export default function SearchPage() {
     <div className="absolute inset-0 bg-[url('/ui/bg.svg')]">
       <div className="mx-auto max-w-3xl absolute inset-4 md:inset-8 bg-white">
         <div className="h-20 pointer-events-none rounded-t-2xl w-full backdrop-filter absolute top-0 bg-gradient-to-t from-transparent to-white [mask-image:linear-gradient(to_bottom,white,transparent)]"></div>
-        <div className="px-4 md:px-8 pt-6 pb-24 rounded-2xl ring-8 ring-zinc-300/20 border border-zinc-200 h-full overflow-auto">
+        <div className="px-4 md:px-8 pt-6 pb-24 rounded-2xl ring-8 ring-zinc-300/20 border border-zinc-200">
           <Title query={query}></Title>
 
           <Results
@@ -38,7 +38,7 @@ export default function SearchPage() {
           />
         </div>
         <div className="h-80 pointer-events-none w-full rounded-b-2xl backdrop-filter absolute bottom-0 bg-gradient-to-b from-transparent to-white [mask-image:linear-gradient(to_top,white,transparent)]"></div>
-        <div className="absolute z-10 flex items-center justify-center bottom-6 px-4 md:px-8 w-full">
+        <div className="fixed mx-auto max-w-3xl inset-x-4 md:inset-x-8 flex items-center justify-center bottom-6 px-4 md:px-8 w-full">
           <div className="w-full">
             <Search
               onSubmit={(e, value) => {
